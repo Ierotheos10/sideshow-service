e82ee5f1131b109d7e5d4652a23e58793fc51c1a
Ierotheos10 <105287381+Ierotheos10@users.noreply.github.com>
2022-05-17 20:08:57 +0300

    Αρχικά αρχεία για έργο MakeCode

---
+++ main.ts
@@ -0,0 +1,31 @@
+input.onButtonPressed(Button.A, function () {
+    keyboard.sendString(keyboard.keys(keyboard._Key.left))
+    basic.showLeds(`
+        . . # . .
+        . # # # #
+        # # # # #
+        . # # # #
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    keyboard.sendString(keyboard.keys(keyboard._Key.right))
+    basic.showLeds(`
+        . . # . .
+        # # # # .
+        # # # # #
+        # # # # .
+        . . # . .
+        `)
+})
+keyboard.startKeyboardService()
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
